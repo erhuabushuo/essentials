2fda8d462b15c87a964000c221bfdc31f498b022
Aidan.He <chuiniubila@foxmail.com>
2021-02-21 16:19:53 +0800

    change env to os.environ

--- src/masonite/contrib/essentials/helpers/views/hashid.py
+++ src/masonite/contrib/essentials/helpers/views/hashid.py
@@ -1,8 +1,10 @@
+import os
+
 from hashids import Hashids
 
 
 def hashid(*values, decode=False, min_length=7):
-    hash_class = Hashids(salt=env('KEY', ''), min_length=min_length)
+    hash_class = Hashids(salt=os.environ.get('KEY', ''), min_length=min_length)
     # decode a dictionary
     if type(values[0]) == dict and decode:
         new_dict = {}
